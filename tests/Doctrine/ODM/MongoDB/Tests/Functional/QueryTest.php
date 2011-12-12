kaiwa/mongodb-odm

commit ee37d65fb73a29c7d80ee8f2adc48665f59f7030

prepare query for embedded expressions to

lib/Doctrine/ODM/MongoDB/Query/Builder.php
@@ -187,10 +187,7 @@ class Builder extends \Doctrine\MongoDB\Query\Builder
 
         $query = $this->query;
 
-        $query['query'] = $this->dm->getUnitOfWork()
-            ->getDocumentPersister($this->class->name)
-            ->prepareQuery($this->expr->getQuery());
-
+        $query['query'] = $this->expr->getQuery();
         $query['newObj'] = $this->expr->getNewObj();
 
         return new Query(
@@ -213,7 +210,10 @@ class Builder extends \Doctrine\MongoDB\Query\Builder
      */
     public function expr()
     {
-        return new Expr($this->dm, $this->cmd);
+        $expr = new Expr($this->dm, $this->cmd);
+        $expr->setClassMetadata($this->class);
+
+        return $expr;
     }
 
     private function setDocumentName($documentName)

lib/Doctrine/ODM/MongoDB/Query/Expr.php
@@ -109,4 +109,11 @@ class Expr extends \Doctrine\MongoDB\Query\Expr
 
         return $this;
     }
+
+    public function getQuery()
+    {
+        return $this->dm->getUnitOfWork()
+            ->getDocumentPersister($this->class->name)
+            ->prepareQuery($this->query);
+    }
 }

tests/Doctrine/ODM/MongoDB/Tests/Query/ExprTest.php
@@ -11,13 +11,31 @@ class ExprTest extends \PHPUnit_Framework_TestCase
         $dm = $this->getMockBuilder('Doctrine\\ODM\\MongoDB\\DocumentManager')
             ->disableOriginalConstructor()
             ->getMock();
+        $uw = $this->getMockBuilder('Doctrine\ODM\MongoDB\UnitOfWork')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $documentPersister = $this->getMockBuilder('Doctrine\ODM\MongoDB\Persisters\DocumentPersister')
+            ->disableOriginalConstructor()
+            ->getMock();
         $class = $this->getMockBuilder('Doctrine\\ODM\\MongoDB\\Mapping\\ClassMetadata')
             ->disableOriginalConstructor()
             ->getMock();
 
+        $expected = array('foo.$id' => '1234');
+
         $dm->expects($this->once())
             ->method('createDBRef')
             ->will($this->returnValue(array('$ref' => 'coll', '$id' => '1234', '$db' => 'db')));
+        $dm->expects($this->once())
+            ->method('getUnitOfWork')
+            ->will($this->returnValue($uw));
+        $uw->expects($this->once())
+            ->method('getDocumentPersister')
+            ->will($this->returnValue($documentPersister));
+        $documentPersister->expects($this->once())
+            ->method('prepareQuery')
+            ->with($expected)
+            ->will($this->returnValue($expected));
         $class->expects($this->once())
             ->method('getFieldMapping')
             ->will($this->returnValue(array('targetDocument' => 'Foo')));
@@ -26,7 +44,7 @@ class ExprTest extends \PHPUnit_Framework_TestCase
         $expr->setClassMetadata($class);
         $expr->field('foo')->references(new \stdClass());
 
-        $this->assertEquals(array('foo.$id' => '1234'), $expr->getQuery(), '->references() uses just $id if a targetDocument is set');
+        $this->assertEquals($expected, $expr->getQuery(), '->references() uses just $id if a targetDocument is set');
     }
 
     public function testReferencesUsesAllKeys()
@@ -34,13 +52,31 @@ class ExprTest extends \PHPUnit_Framework_TestCase
         $dm = $this->getMockBuilder('Doctrine\\ODM\\MongoDB\\DocumentManager')
             ->disableOriginalConstructor()
             ->getMock();
+        $uw = $this->getMockBuilder('Doctrine\ODM\MongoDB\UnitOfWork')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $documentPersister = $this->getMockBuilder('Doctrine\ODM\MongoDB\Persisters\DocumentPersister')
+            ->disableOriginalConstructor()
+            ->getMock();
         $class = $this->getMockBuilder('Doctrine\\ODM\\MongoDB\\Mapping\\ClassMetadata')
             ->disableOriginalConstructor()
             ->getMock();
 
+        $expected = array('foo.$ref' => 'coll', 'foo.$id' => '1234', 'foo.$db' => 'db');
+
         $dm->expects($this->once())
             ->method('createDBRef')
             ->will($this->returnValue(array('$ref' => 'coll', '$id' => '1234', '$db' => 'db')));
+        $dm->expects($this->once())
+            ->method('getUnitOfWork')
+            ->will($this->returnValue($uw));
+        $uw->expects($this->once())
+            ->method('getDocumentPersister')
+            ->will($this->returnValue($documentPersister));
+        $documentPersister->expects($this->once())
+            ->method('prepareQuery')
+            ->with($expected)
+            ->will($this->returnValue($expected));
         $class->expects($this->once())
             ->method('getFieldMapping')
             ->will($this->returnValue(array()));
@@ -49,6 +85,6 @@ class ExprTest extends \PHPUnit_Framework_TestCase
         $expr->setClassMetadata($class);
         $expr->field('foo')->references(new \stdClass());
 
-        $this->assertEquals(array('foo.$ref' => 'coll', 'foo.$id' => '1234', 'foo.$db' => 'db'), $expr->getQuery(), '->references() uses all keys if no targetDocument is set');
+        $this->assertEquals($expected, $expr->getQuery(), '->references() uses all keys if no targetDocument is set');
     }
 }

commit f4afcb2de3953f75d5346f322d28afd53784c883

[#204] Add failing QueryBuilder elemMatch() test case
This demonstrates a regression (likely introduced in ee37d65fb73a29c7d80ee8f2adc48665f59f7030) that breaks elemMatch() for deep field paths in embedded objects. The actual error comes from DocumentPersister::prepareQueryValue(), which attempts to validate the field path on the document class since it contains a dot separator. Although fields without a separator might otherwise be ignored, the preparation method still incorrectly checks the path against the top-level document class (which might lead to incorrect identifier processing).

tests/Doctrine/ODM/MongoDB/Tests/Functional/QueryTest.php
@@ -41,6 +41,27 @@ class QueryTest extends \Doctrine\ODM\MongoDB\Tests\BaseTest
         $this->assertNotNull($user);
     }
 
+    public function testAddElemMatchWithDeepFields()
+    {
+        $user1 = new User();
+        $user1->setUsername('ben');
+
+        $user2 = new User();
+        $user2->setUsername('boo');
+        $phonenumber = new Phonenumber('2125550123', $user1);
+        $user2->addPhonenumber($phonenumber);
+
+        $this->dm->persist($user1);
+        $this->dm->persist($user2);
+        $this->dm->flush();
+
+        $qb = $this->dm->createQueryBuilder('Documents\User');
+        $qb->field('phonenumbers')->elemMatch($qb->expr()->field('lastCalledBy.$id')->equals(new \MongoId($user1->getId())));
+        $query = $qb->getQuery();
+        $user = $query->getSingleResult();
+        $this->assertNotNull($user);
+    }
+
     public function testAddNot()
     {
         $user = new User();

tests/Documents/Phonenumber.php
@@ -11,9 +11,13 @@ class Phonenumber
     /** @ODM\String */
     private $phonenumber;
 
-    public function __construct($phonenumber = null)
+    /** @ODM\ReferenceOne(targetDocument="User", cascade={"persist"}) */
+    private $lastCalledBy;
+
+    public function __construct($phonenumber = null, User $lastCalledBy = null)
     {
         $this->phonenumber = $phonenumber;
+        $this->lastCalledBy = $lastCalledBy;
     }
 
     public function getPhonenumber()
@@ -24,5 +28,15 @@ class Phonenumber
     public function setPhonenumber($phonenumber)
     {
         $this->phonenumber = $phonenumber;
+    }
+
+    public function getLastCalledBy()
+    {
+        return $this->lastCalledBy;
+    }
+
+    public function setLastCalledBy(User $lastCalledBy = null)
+    {
+        $this->lastCalledBy = $lastCalledBy;
     }
 }